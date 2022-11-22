84267d4a6c170f9fea2c6918f4a27ad95809042c
prince475 <danielmwai475@gmail.com>
2022-11-22 09:15:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+//const is used for unchanging variable
+const companyName = 'Scuber'
+//let has an advantage in that variables can be reassigned a value to itle  
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+    
